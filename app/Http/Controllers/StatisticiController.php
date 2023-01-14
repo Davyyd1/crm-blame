dca8aef67a0d519ce58ce1b3c48126c113e15945
Davyyd1 <d.dav889@gmail.com>
2023-01-14 18:58:14 +0200

    statistici(stats)

--- app/Http/Controllers/StatisticiController.php
+++ app/Http/Controllers/StatisticiController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatisticiController extends Controller
 {
     public function viewStatistici()
     {
-        return view('statistici');
+        $cheltuieli23 = DB::table('IstoricProiecte')->select('suma')->where('action_type', '=', 'cheltuiala')->whereYear('data', '=', "2023")->get()->sum('suma') * -1;
+        $plati23 = DB::table('IstoricProiecte')->select('suma')->where('action_type', '=', 'plata')->whereYear('data', '=', "2023")->get()->sum('suma');
+        $incasari23 = DB::table('IstoricProiecte')->select('suma')->where('action_type', '=', 'incasare')->whereYear('data', '=', "2023")->get()->sum('suma');
+        return view('statistici', compact('cheltuieli23', 'plati23', 'incasari23'));
     }
 }
--- resources/views/statistici.blade.php
+++ resources/views/statistici.blade.php
@@ -13,6 +13,35 @@
                                 <h2>Statistici</h2><hr>
                             </div>
                             
+                            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+                            <script type="text/javascript">
+                            google.charts.load('current', {'packages':['bar']});
+                            google.charts.setOnLoadCallback(drawChart);
+
+                            function drawChart() {
+                                var data = google.visualization.arrayToDataTable([
+                                ['An', 'Incasari', 'Cheltuieli', 'Plati'],
+                                ['2023', {{ $incasari23 }}, {{ $cheltuieli23 }}, {{ $plati23 }}],
+                                ['2024', 0, 0, 0],
+                                ['2025', 0,0,0],
+                                ['2026', 0,0,0],
+                                ['2027', 0,0,0],
+                                ['2028', 0,0,0]
+                                ]);
+
+                                var options = {
+                                chart: {
+                                    title: 'Performanta Companiei',
+                                    subtitle: 'Incasari, Cheltuieli, si Plati: 2023-2028',
+                                }
+                                };
+
+                                var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                                chart.draw(data, google.charts.Bar.convertOptions(options));
+                            }
+                            </script>
+                            <div id="columnchart_material" style="width: 77rem; height: 40rem;"></div>
                         </div>
                     </div>
                 </div>
